0122f745626a1bdedfa1bb15f76489e513ef3cf6
NaturalMethods <nathansanchezpro@gmail.com>
2026-04-18 12:29:12 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './index.css'
+import App from './App.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <Router>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/dashboard" element={<dashboard />} />
+              <Route path="/profile" element={<profile />} />
+              <Route path="/test" element={<h1>Error</h1>} />
+          </Routes>
+      </Router>
+  </StrictMode>,
+)
